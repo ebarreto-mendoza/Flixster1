1dc1f2f854a1ccad7277156ae278a52a19a3020d
ebarreto-mendoza <60796295+ebarreto-mendoza@users.noreply.github.com>
2021-01-31 21:30:10 -0800

    Flixster

---
+++ Flixster/MovieViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieViewCell.swift
+//  Flixster
+//
+//  Created by Eduardo Barreto-Mendoza on 1/31/21.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
